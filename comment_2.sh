16d3f3cf24860ece1a50f6e3a74c6b64b4c7621e
TUSHAR KANT SAO <77832400+Tushar-2510@users.noreply.github.com>
2023-07-07 15:30:01 +0530

    Update comment_2.sh

--- comment_2.sh
+++ comment_2.sh
@@ -1,36 +1,9 @@
-#!/bin/bash
-echo "Hey! We have found that this repository uses the following languages:" >> comment.txt
-# Read the languages.txt file and store the languages in an array
-languages=()
-while IFS= read -r line; do
-  languages+=("$line")
-done < languages.txt
-echo "${languages[@]}" >> comment.txt
-echo "And according to our database, these files are required to run the project:" >> comment.txt
-echo 
-echo
-#echo languages: "${languages[@]}"
-# Read the dependency.txt file and check if each language is present
-while IFS= read -r line; do
-  # Extract the language name and requirement file
-    language="$(echo "$line" | sed -E 's/^([[:alnum:]\+\#\.]+)\s+.*$/\1/')"
-    #echo "Language: $language"
-    # Check if the language is present in the languages array
-    for lang in "${languages[@]}"; do
-      if [[ "$language" == "$lang" ]]; then
-      echo "$line" >> comment.txt
-      fi
-    done
-done < dependency.txt
-echo "------------------" >> comment.txt
-echo "If you want us to keep track of these files for latest updates, close the issue with the filename in the comment." >> comment.txt
-
 REPO_URL="https://github.com/Sopra-Banking-Software-Interns/Language-agnostic-model"
 
 # Issue title and body
-ISSUE_TITLE="Dependency check for codespace"
+ISSUE_TITLE="Depedency Update Alert"
 
-mapfile -t lines < comment.txt
+mapfile -t lines < version_updates.txt
 
 # Concatenate the lines into the issue body
 ISSUE_BODY=""
@@ -71,6 +44,3 @@ fi
 
 # Display the created issue number
 echo "New issue created: #$issue_number"
-rm comment.txt
-rm languages.txt
-
