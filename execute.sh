329a4cfadcbcfc7f723e9ee5c80ae20799f4bc9f
Priyansh Shrivastava <priyansh.shrivastava@ext.soprabanking.com>
2023-07-07 16:32:34 +0530

    reverts execute to working file

--- execute.sh
+++ execute.sh
@@ -1,56 +1,56 @@
 linenumber=$(sed -n '$=' dependency_present.txt)
-while IFS= read -r linew
+for (( x=1; x<=$linenumber; x++ ))
 do
-  if [[ $linew == "requirements.txt" ]]
-  then
-    chmod +x ./python_req.sh
-    ./python_req.sh
-  fi
-  if [[ $linew == "packages.json" ]]
-  then
-    chmod +x ./node.sh
-    ./node.sh
-  fi
-  if [[ $linew == "build.gradle" ]]
-  then
-    chmod +x ./gradle.sh
-    ./gradle.sh
-  fi
-  if [[ $linew == "pom.xml" ]]
-  then
-    chmod +x ./maven.sh
-    ./maven.sh
-  fi
-  if [[ $linew == "gemfile" ]]
-  then
-    chmod +x ./ruby_gemfile.sh
-    ./ruby_gemfile.sh
-  fi
-  if [[ $linew == "cargo.toml" ]]
-  then
-    chmod +x ./cargo.sh
-    ./cargo.sh
-  fi
-  if [[ $linew == "package.yaml" ]]
-  then
-    chmod +x ./haskell.sh
-    ./haskell.sh
-  fi
-  if [[ $linew == "composer.json" ]]
-  then
-    chmod +x ./php.sh
-    ./php.sh
-  fi
-  if [[ $linew == "package.swift" ]]
-  then
-    chmod +x ./swift.sh
-    ./swift.sh
-  fi
-  if [[ $linew == "packages.config" ]]
-  then
-    chmod +x ./csharp.sh
-    ./csharp.sh
-  fi
-done < dependency_present.txt
-
+linew=$(sed -n "${x}p" dependency_present.txt)
+if [[ $linew == "requirements.txt" ]]
+then
+chmod +x ./python_req.sh
+./python_req.sh
+fi
+if [[ $linew == "packages.json" ]]
+then
+chmod +x ./node.sh
+./node.sh
+fi
+if [[ $linew == "build.gradle" ]]
+then
+chmod +x ./gradle.sh
+./gradle.sh
+fi
+if [[ $linew == "pom.xml" ]]
+then
+chmod +x ./maven.sh
+./maven.sh
+fi
+if [[ $linew == "gemfile" ]]
+then
+chmod +x ./ruby_gemfile.sh
+./ruby_gemfile.sh
+fi
+if [[ $linew == "cargo.toml" ]]
+then
+chmod +x ./cargo.sh
+./cargo.sh
+fi
+if [[ $linew == "package.yaml" ]]
+then
+chmod +x ./haskell.sh
+./haskell.sh
+fi
+if [[ $linew == "composer.json" ]]
+then
+chmod +x ./php.sh
+./php.sh
+fi
+if [[ $linew == "package.swift" ]]
+then
+chmod +x ./swift.sh
+./swift.sh
+fi
+if [[ $linew == "packages.config" ]]
+then
+chmod +x ./csharp.sh
+./csharp.sh
+fi
+done
 rm dependency_present.txt
