faf30c03f6991db3927f043bf1efcd605ca7ed2f
Suvarchala-30 <134471094+Suvarchala-30@users.noreply.github.com>
2023-07-07 14:34:12 +0530

    Create php.sh

---
+++ php.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+jq '.require' composer.json > depe.json
+
+# Read the dependencies.json file
+dependencies=$(cat depe.json )
+echo -n "" > latest_versions.txt
+# Create an array to store the latest versions
+declare -A latest_versions
+
+# Loop through each dependency and check the latest version
+while IFS=':' read -r dependency version; do
+    # Remove leading/trailing whitespace and quotes from the dependency and version
+    dependency=$(echo "$dependency" | tr -d '[:space:]"')
+    version=$(echo "$version" | tr -d '[:space:]"')
+
+    # Use cargo search command to check the latest version
+    latest_version=$(composer show -a $dependency | grep -oE 'versions : [^,]+' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+    
+    # Store the latest version in the array
+    latest_versions["$dependency"]=$latest_version
+
+    # Compare the version from dependencies.json with the latest version
+    if [[ "$version" != "$latest_version" ]]; then
+        echo "Update available for $dependency = $latest_version" >> latest_versions.txt
+    fi
+done <<< "$dependencies"
+
+echo "$(cat latest_versions.txt)" >> version_changes.txt
+rm latest_versions.txt
+rm depe.json
+
