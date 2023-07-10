2c84a11546beb0c11f83e0511dc77e1f0852ae5d
TUSHAR KANT SAO <77832400+Tushar-2510@users.noreply.github.com>
2023-07-10 15:20:08 +0530

    Update csharp.sh

--- csharp.sh
+++ csharp.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
   echo "------------------" >> version_updates.txt
   echo "C# (packages.config) Dependenct alert" >> version_updates.txt
   echo "------------------" >> version_updates.txt
@@ -7,7 +7,8 @@ PACKAGES_CONFIG="packages.config"
 # Read the packages.config file and extract package names and versions
 packages=($(grep -oP '(?<=<package id=")[^"]+' $PACKAGES_CONFIG))
 versions=($(grep -oP '(?<=version=")[^"]+' $PACKAGES_CONFIG))
-
+echo "$packages"
+echo "$versions"
 # Loop through the packages and check for the latest versions
 for ((i=0; i<${#packages[@]}; i++)); do
     package="${packages[i]}"
@@ -16,7 +17,7 @@ for ((i=0; i<${#packages[@]}; i++)); do
     # Get the latest version for the package
   #  latest_version=$(nuget list $package -AllVersions | awk '{print $2}' | tail -n 1)
  latest_version=$(curl -s "https://api.nuget.org/v3-flatcontainer/$package/index.json" | jq '.versions[-1]')
-echo "$latest_version" >> myfile.txt
+echo "$latest_version"
     # Compare the current version with the latest version
     if [[ "$version" != "$latest_version" ]]; then
         echo -n "Package: $package " >> version_updates.txt
