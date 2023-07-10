1b0f90316fb5afb7726180d13dd18779a08f7346
TUSHAR KANT SAO <77832400+Tushar-2510@users.noreply.github.com>
2023-07-10 15:03:08 +0530

    Create csharp.sh

---
+++ csharp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Path to the packages.config file
+PACKAGES_CONFIG="packages.config"
+# Read the packages.config file and extract package names and versions
+packages=($(grep -oP '(?<=<package id=")[^"]+' $PACKAGES_CONFIG))
+versions=($(grep -oP '(?<=version=")[^"]+' $PACKAGES_CONFIG))
+
+# Loop through the packages and check for the latest versions
+for ((i=0; i<${#packages[@]}; i++)); do
+    package="${packages[i]}"
+    version="${versions[i]}"
+
+    # Get the latest version for the package
+  #  latest_version=$(nuget list $package -AllVersions | awk '{print $2}' | tail -n 1)
+ latest_version=$(curl -s "https://api.nuget.org/v3-flatcontainer/$package/index.json" | jq '.versions[-1]')
+echo "$latest_version" >> myfile.txt
+    # Compare the current version with the latest version
+    if [[ "$version" != "$latest_version" ]]; then
+        echo -n "Package: $package " >> version_updates.txt
+        echo -n "Current version: $version " >> version_updates.txt
+        echo -n "Latest version: $latest_version" >> version_updates.txt
+        echo >> version_updates.txt
+    fi
+done
