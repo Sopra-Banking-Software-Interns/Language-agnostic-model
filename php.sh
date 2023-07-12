f29a9e154fb92fd1b53415c817121ec59dab3d4d
TUSHAR KANT SAO <77832400+Tushar-2510@users.noreply.github.com>
2023-07-12 15:25:52 +0530

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-echo -n "" > version_updates.txt  # Add this line to create the file
-echo -n "" > latest_versions.txt
-echo -n "" > depe.json
+#!/bin/bash 
+# Add this line to create the file
+
+
     echo "------------------" >> version_updates.txt
     echo "PHP (composer.json) Dependency alert" >> version_updates.txt
     echo "------------------" >> version_updates.txt
 jq '.require' composer.json > depe.json
 # Read the dependencies.json file
 dependencies=$(cat depe.json )
-echo -n "" > latest_versions.txt
 # Create an array to store the latest versions
 declare -A latest_versions
 # Loop through each dependency and check the latest version
@@ -22,16 +21,15 @@ while IFS=':' read -r dependency version; do
     # Use cargo search command to check the latest version
     latest_version=$(composer show -a $dependency | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | head -1)
 
- 
-
     # Store the latest version in the array
     latest_versions["$dependency"]=$latest_version
-
+echo 
  
 
     # Compare the version from dependencies.json with the latest version
     if [[ "$version" < "$latest_version" ]]; then
         echo "Update available for $dependency = $latest_version" >> latest_versions.txt
+        echo "$latest_version"
     fi
 done <<< "$dependencies"
 
@@ -46,6 +44,3 @@ echo
 while read line; do
     echo $line
 done < version_updates.txt
-
-rm latest_versions.txt
-rm depe.json
