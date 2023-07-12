61b19580506c058aefd0f634388b4b14fa367a27
Suvarchala-30 <134471094+Suvarchala-30@users.noreply.github.com>
2023-07-12 12:37:26 +0530

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
-jq '.require' composer.json > depe.json
-
-# Read the dependencies.json file
-dependencies=$(cat depe.json )
-echo -n "" > latest_versions.txt
 echo -n "" > version_updates.txt  # Add this line to create the file
     echo "------------------" >> version_updates.txt
     echo "PHP (composer.json) Dependency alert" >> version_updates.txt
     echo "------------------" >> version_updates.txt
+jq '.require' composer.json > depe.json
+# Read the dependencies.json file
+dependencies=$(cat depe.json )
+echo -n "" > latest_versions.txt
 # Create an array to store the latest versions
 declare -A latest_versions
-
 # Loop through each dependency and check the latest version
 while IFS=':' read -r dependency version; do
     # Remove leading/trailing whitespace and quotes from the dependency and version
     dependency=$(echo "$dependency" | tr -d '[:space:]"')
     version=$(echo "$version" | tr -d '[:space:]"')
-    #echo $dependency - $version
-    
+
+ 
+
     # Use cargo search command to check the latest version
     latest_version=$(composer show -a $dependency | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | head -1)
-    #echo $latest_version
-    
+
+ 
+
     # Store the latest version in the array
     latest_versions["$dependency"]=$latest_version
 
+ 
+
     # Compare the version from dependencies.json with the latest version
-    if [[ "$version" != "$latest_version" ]]; then
-        line="Update available for $dependency with latest version $latest_version"
-        echo $line >> version_updates.txt
-        echo $line
+    if [[ "$version" < "$latest_version" ]]; then
+        echo "Update available for $dependency = $latest_version" >> latest_versions.txt
     fi
-done < "$dependencies"
+done <<< "$dependencies"
+
+echo "$(cat latest_versions.txt)" >> version_updates.txt
+rm latest_versions.txt
 rm depe.json
-    echo "------------------" >> version_updates.txt
-echo 
-echo 
-echo "Reading File"
-echo 
-echo 
-while read line; do
-    echo $line
-done < version_updates.txt
